c1d9e21d702feca1cd3ba10a099ad5d4ad344b46
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-10-27 22:15:26 +0200

    [?^] [Emit] Debugging session..
    Added commented debug code. (And testing tool in another commit.)
    
    What I observe is:
    
      * Signal duration is depends of frequency. 50 kHz is fine, 500 kHz
        is bad - signal too long
      * Duration depends of number of microseconds from GetTime_Precise().
        If current micros is over 900, our signal will trim right after
        main delay. Not doing any precise parking.
    
    I can not explain this. Just documenting what I observe.

--- src/me_ModulatedSignalPlayer.cpp
+++ src/me_ModulatedSignalPlayer.cpp
@@ -2,16 +2,23 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2025-10-26
+  Last mod.: 2025-10-27
 */
 
 #include <me_ModulatedSignalPlayer.h>
 
 #include <me_BaseTypes.h>
-#include <me_Duration.h>
 
+#include <me_Duration.h>
 #include <me_FrequencyGenerator.h>
 #include <me_Delays.h>
+#include <me_RunTime.h>
+
+#include <avr/common.h>
+#include <avr/interrupt.h>
+
+// #include <me_Console.h>
+// #include <me_DebugPrints.h>
 
 using namespace me_ModulatedSignalPlayer;
 
@@ -36,16 +43,76 @@ void me_ModulatedSignalPlayer::Emit(
   me_Duration::TDuration Duration
 )
 {
-  const me_Duration::TDuration EmitOverhead = { 0, 0, 0, 110 };
+  const me_Duration::TDuration LastSnatch = { 0, 0, 0, 280 };
+  const TUint_2 LastSnatchOverhead_Us = 26;
 
-  if (!me_Duration::Subtract(&Duration, EmitOverhead))
+  me_Duration::TDuration StartTime;
+  me_Duration::TDuration CurTime;
+  me_Duration::TDuration EndTime;
+  me_Duration::TDuration TimeRemained;
+  TUint_2 LastSnatch_Us;
+
+  TUint_1 OrigSreg;
+
+  StartTime = me_RunTime::GetTime_Precise();
+
+  EndTime = StartTime;
+  if (!me_Duration::Add(&EndTime, Duration))
+    return;
+
+  if (!me_Duration::Subtract(&Duration, LastSnatch))
     Duration = me_Duration::Zero;
 
   me_FrequencyGenerator::StartFreqGen();
+
   me_Delays::Delay_Duration(Duration);
+
+  OrigSreg = SREG;
+  cli();
+
+  CurTime = me_RunTime::GetTime_Precise();
+
+  TimeRemained = EndTime;
+  if (!me_Duration::Subtract(&TimeRemained, CurTime))
+    TimeRemained = me_Duration::Zero;
+  // Expectation: <TimeRemained> is between (<LastSnatch>, 1 ms)
+
+  if (TimeRemained.MicroS > LastSnatchOverhead_Us)
+    LastSnatch_Us = TimeRemained.MicroS - LastSnatchOverhead_Us;
+  else
+    LastSnatch_Us = 0;
+
+  me_Delays::Delay_Us(LastSnatch_Us);
+
   me_FrequencyGenerator::StopFreqGen();
+
+  SREG = OrigSreg;
+
+  /*
+  Console.Write("StartTime");
+  me_DebugPrints::PrintDuration(StartTime);
+  Console.EndLine();
+
+  Console.Write("CurTime");
+  me_DebugPrints::PrintDuration(CurTime);
+  Console.EndLine();
+
+  Console.Write("EndTime");
+  me_DebugPrints::PrintDuration(EndTime);
+  Console.EndLine();
+
+  Console.Write("TimeRemained");
+  me_DebugPrints::PrintDuration(TimeRemained);
+  Console.EndLine();
+
+  me_DebugPrints::Print("Last snatch (us)", LastSnatch_Us);
+  Console.EndLine();
+
+  Console.EndLine();
+  */
 }
 
 /*
   2025-09-15
+  2025-10-27
 */
